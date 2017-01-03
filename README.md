c65731987456d0f913f164b1836a47f19a1b1211
Jerry <jerry@JerrysMBPRetina.hitronhub.home>
2017-01-03 14:48:16 -0800

    Pushing to github.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Course
+https://www.pluralsight.com/courses/django-fundamentals
+
+# Techonogies
+Python, Django, Twitter Bootstrap, SQLite
---
+++ tictactoe/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+import tictactoe.views
+
+from .views import AllGamesList
+
+urlpatterns = [
+    url(r'^invite$', tictactoe.views.new_invitation, name='tictactoe_invite'),
+    url(r'invitation/(?P<pk>\d+)/$',
+        tictactoe.views.accept_invitation,
+        name='tictactoe_accept_invitation'),
+    url(r'^game/(?P<pk>\d+)/$',
+        tictactoe.views.game_detail,
+        name='tictactoe_game_detail'),
+    url(r'^game/(?P<pk>\d+)/do_move$',
+        tictactoe.views.game_do_move,
+        name='tictactoe_game_do_move'),
+    url(r'^game/all', AllGamesList.as_view(), name='tictactoe_all_games')
+]
